26920aeeff46a6d14e629b8856c834b1ed665b2e
Emmanuel Deshommes <emmanxdesh@gmail.com>
2021-10-27 15:58:36 -0400

    Full Register UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,214 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  //WidgetsApp //MaterialApp //CupertinoApp
+  runApp(MaterialApp(
+    home: RegisterPage(),
+
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    Color backgroundColor = const Color.fromARGB(255, 36, 37, 45);
+    Color tFocusColor = const Color.fromARGB(255, 117, 20, 141);
+    Color tFillColor = const Color.fromARGB(255, 45, 45, 45);
+    Color buttonColor1 = const Color.fromARGB(255, 142, 63, 255);
+    Color buttonColor2 = const Color.fromARGB(255, 181, 55, 254);
+    const String title = "Sign Up";
+    const String email = "Email";
+    const String username = "Username";
+    const String password = "Password";
+    const String button = "Sign Up";
+    final userNameCont = TextEditingController();
+    final emailCont = TextEditingController();
+    final passwordCont = TextEditingController();
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+
+      body: Container(
+        color: backgroundColor,
+
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Container(
+                padding: EdgeInsets.only(),
+
+                child: Container(
+                  margin: EdgeInsets.only(left: 50),
+
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,          
+                    children: [
+                      IconButton(onPressed: (){}, icon: Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.white,
+                        size: 20,
+                      )),
+                      Text(
+                        title,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                padding: EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 200,
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(right: 200, top: 0),
+                child: Text(
+                  username,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.fromLTRB(50, 10, 50, 20),
+                child: TextField(
+                  controller: userNameCont,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    fillColor: tFillColor,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: tFocusColor),
+                      borderRadius: BorderRadius.circular(15.0)
+                    ),
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(right: 220),
+                child: Text(
+                  email,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.fromLTRB(50, 10, 50, 20),
+
+                child: TextField(
+                  controller: emailCont,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+
+                  decoration: InputDecoration(
+                    fillColor: tFillColor,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    focusColor: Colors.black,
+
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: tFocusColor),
+                      borderRadius: BorderRadius.circular(15.0)
+                    )
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(right: 200),
+                child: Text(
+                  password,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+
+              Container(
+                padding: EdgeInsets.fromLTRB(50, 10, 50, 20),
+                child: TextField(
+                  obscureText: true,
+                  controller: passwordCont,
+
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  
+                  decoration: InputDecoration(
+                    
+                    filled: true,
+                    fillColor: tFillColor,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(15.0)
+                    ),
+
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: tFocusColor),
+                      borderRadius: BorderRadius.circular(15.0),
+                    )
+                  ),
+                ),
+              ),
+
+              GestureDetector(
+                child: Container(
+                  margin: EdgeInsets.only(top: 50),
+                  padding: EdgeInsets.fromLTRB(110, 20, 110, 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [buttonColor1, buttonColor2],
+                    ),
+                  ),
+                  child: Text(
+                    button,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+
+                onTap: () {
+                  print(userNameCont.text);
+                },
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
